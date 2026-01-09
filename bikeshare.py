0f82b6c999bb904813f709b5cf4708a136e59f2b
Ivars Knets <ivars.knets@accenture.com>
2026-01-09 17:52:08 +0200

    project fixes
    - Changed so some inputs support "all"
    - Added check to see if dataFrame has any data
    - Fixed variable naming
    - Fixed wrong const values
    - Pretied up some data (dates, line input for raw data display)

--- bikeshare.py
+++ bikeshare.py
@@ -1,12 +1,65 @@
 import time
 import pandas as pd
-import numpy as np
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
-month_names = ['january', 'february', 'march', 'april', 'may', 'june'] #, 'july', 'august', 'september', 'october', 'november', 'december'
-day_names = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+CITY_DATA = { 
+    'chicago': 'chicago.csv',
+    'new york city': 'new_york_city.csv',
+    'washington': 'washington.csv'
+}
+MONTH_NAMES = ['january', 'february', 'march', 'april', 'may', 'june'] #, 'july', 'august', 'september', 'october', 'november', 'december']
+DAY_NAMES = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+
+
+def validatedInput(message, checkList, allIsValidInput=False):
+    """
+    An input with validation
+
+    Inputs:
+        message (str): message that informs what to input
+        checkList (list): list of acceptable inputs
+        allIsValidInput (boolean): boolean showing if "all" should be a valid input
+
+    Output:
+        (str): the input string
+
+    """
+    result = None
+    while result == None:
+        user_input = input(message)
+        if user_input.lower() in checkList or (allIsValidInput and user_input.lower() == 'all'):
+            result = user_input.lower()
+        else: 
+            print('Invalid input. Try again!')
+
+    return result
+
+
+def validatedIntInput(message, default):
+    """
+    An input that accepts only integers or empty string (for default value)
+
+    Inputs:
+        message (str): message that informs what to input
+        default (int): default int value, to use when empty string is input
+
+    Output:
+        (int): the input string converted to int
+        
+    """
+    result = None
+
+    while result == None:
+        user_input = input(message)        
+
+        if user_input.isdigit():
+            result = user_input
+        elif user_input == '':
+            result = default
+        else: 
+            print('Invalid input. Try again!')
+          
+    return int(result)
 
 
 def access_df_col(df, col):
@@ -21,10 +74,41 @@ def access_df_col(df, col):
         (pd.Series/None): accessed column data as a pd.Series
     """
     if col in df.columns:
-        return df[[col]]
+        result = df[[col]]
+        if result.empty:
+            print(f'No data available for column "{col}"')
+            return None
+        else: 
+            return result
     else:
         print(f'\n--- {col}: no such column available')
         return None
+
+
+def seconds_to_readable(secs):
+    secs_as_int = None
+
+    if isinstance(secs, pd.Series):
+        secs_as_int = int(secs.iloc[0])
+    else:
+        secs_as_int = int(secs)
+
+    secs_in_a_year = 31536000
+    full_years = secs_as_int // secs_in_a_year
+    remaining_secs_after_years = secs_as_int % secs_in_a_year
+
+    secs_in_a_day = 86400
+    full_days = remaining_secs_after_years // secs_in_a_day
+    remaining_secs_after_days = secs_as_int % secs_in_a_day
+
+    secs_to_subYear_time = time.strftime('%H hours, %M minutes and %S seconds', time.gmtime(remaining_secs_after_days))
+
+    if full_years > 0:
+        return f'{full_years} years, {full_days} days, {secs_to_subYear_time} ({secs_as_int} seconds)'
+    elif full_years == 0 and full_days > 0:
+        return f'{full_days} days, {secs_to_subYear_time} ({secs_as_int} seconds)'
+    else:
+        return f'{secs_to_subYear_time} ({secs_as_int} seconds)'
 
 
 
@@ -40,20 +124,9 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
-    def validatedInput(text, checkList):
-        result = None
-        while result == None:
-            user_input = input(text)
-            if user_input.lower() in checkList:
-                result = user_input.lower()
-            else: 
-                print('Invalid input. Try again!')
-        return result
-
-
-    input_city = validatedInput('Input a city (chicago, new york city, washington): ', CITY_DATA.keys())
-    input_month = validatedInput('Input month name (between "january" and "june"): ', month_names)
-    input_day = validatedInput('Input day name (eg. "friday"): ', day_names)
+    input_city = validatedInput(f'Input a city ({', '.join(CITY_DATA.keys())}): ', CITY_DATA.keys())
+    input_month = validatedInput(f'Input name of month (between "{MONTH_NAMES[0]}" and "{MONTH_NAMES[-1]}") ("all" is acceptable): ', MONTH_NAMES, True)
+    input_day = validatedInput('Input name of day (eg. "friday") ("all" is acceptable): ', DAY_NAMES, True)
 
 
     print('-'*40)
@@ -84,14 +157,12 @@ def load_data(city, month, day):
 
     # filter by month if applicable
     if month != 'all':
-        months = ['january', 'february', 'march', 'april', 'may', 'june']
-        month = months.index(month) + 1
+        month = MONTH_NAMES.index(month) + 1
         df = df[df['Month'] == month]
 
     # filter by day of week if applicable
     if day != 'all':
-        day_names = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-        day_index = day_names.index(day)
+        day_index = DAY_NAMES.index(day)
         df = df[df['Day of Week'] == day_index]
     
     return df
@@ -103,19 +174,21 @@ def time_stats(df):
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
-    # display the most common month
-    most_common_month = pd.to_datetime(df['Start Time']).dt.month.mode().values[0]
-    print(f'The most common month for travel: \n\t{month_names[most_common_month-1]}')
+    df_start_time = access_df_col(df, 'Start Time')
+    if isinstance(df_start_time, pd.DataFrame):
+        # display the most common month
+        most_common_month = pd.to_datetime(df['Start Time']).dt.month.mode().values[0]
+        print(f'The most common month for travel: \n\t{MONTH_NAMES[most_common_month-1]}')
 
 
-    # display the most common day of week
-    most_common_day = pd.to_datetime(df['Start Time']).dt.day_of_week.mode().values[0]
-    print(f'The most common day of the week for travel: \n\t{day_names[most_common_day]}')
+        # display the most common day of week
+        most_common_day = pd.to_datetime(df['Start Time']).dt.day_of_week.mode().values[0]
+        print(f'The most common day of the week for travel: \n\t{DAY_NAMES[most_common_day]}')
 
 
-    # display the most common start hour
-    most_common_hour = pd.to_datetime(df['Start Time']).dt.hour.mode().values[0]
-    print(f'The most common start hour for travel: \n\t{most_common_hour}')
+        # display the most common start hour
+        most_common_hour = pd.to_datetime(df['Start Time']).dt.hour.mode().values[0]
+        print(f'The most common start hour for travel: \n\t{most_common_hour}')
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -129,20 +202,23 @@ def station_stats(df):
     start_time = time.time()
 
     # display most commonly used start station
-    most_common_startStation = df['Start Station'].value_counts().idxmax()
-    print(f'The most common starting station: \n\t{most_common_startStation}')
+    df_start_stations = access_df_col(df, 'Start Station')
+    if isinstance(df_start_stations, pd.DataFrame):
+        print(f'The most common starting station: \n\t{df_start_stations.value_counts().idxmax()[0]}')
 
 
     # display most commonly used end station
-    most_common_endStation = df['End Station'].value_counts().idxmax()
-    print(f'The most common ending station: \n\t{most_common_endStation}')
+    df_end_stations = access_df_col(df, 'End Station')
+    if isinstance(df_end_stations, pd.DataFrame):
+        print(f'The most common ending station: \n\t{df_end_stations.value_counts().idxmax()[0]}')
 
 
     # display most frequent combination of start station and end station trip
-    most_common_station_combo = df[['Start Station', 'End Station']].value_counts().idxmax()
-    print(f'The most common combination of starting and ending stations:')
-    print(f'\tStarting station: {most_common_station_combo[0]}')
-    print(f'\tEnding station: {most_common_station_combo[1]}')
+    if isinstance(df_start_stations, pd.DataFrame) and isinstance(df_end_stations, pd.DataFrame):
+        most_common_station_combo = df[['Start Station', 'End Station']].value_counts().idxmax()
+        print(f'The most common combination of starting and ending stations:')
+        print(f'\tStarting station: {most_common_station_combo[0]}')
+        print(f'\tEnding station: {most_common_station_combo[1]}')
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -155,15 +231,17 @@ def trip_duration_stats(df):
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
-    df['Travel Time'] = pd.to_datetime(df['End Time']) - pd.to_datetime(df['Start Time'])
 
-    # display total travel time
-    total_travel_time = df['Travel Time'].sum()
-    print(f'Total travel time: \n\t{total_travel_time}')
+    df_trip_duration = access_df_col(df, 'Trip Duration')
+    if isinstance(df_trip_duration, pd.DataFrame):
 
-    # display mean travel time
-    mean_travel_time = df['Travel Time'].mean()
-    print(f'Mean travel time: \n\t{mean_travel_time}')
+        # display total travel time
+        total_travel_time = df_trip_duration.sum()
+        print(f'Total travel time: \n\t{seconds_to_readable(total_travel_time)}')
+
+        # display mean travel time
+        mean_travel_time = df_trip_duration.mean()
+        print(f'Mean travel time: \n\t{seconds_to_readable(mean_travel_time)}')
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -177,24 +255,24 @@ def user_stats(df):
     start_time = time.time()
 
     # Display counts of user types
-    user_per_type = access_df_col(df, 'User Type')
-    if isinstance(user_per_type, pd.DataFrame):
-        print(f'\nCount of users per type: \n{user_per_type.value_counts(dropna= False).to_string(header= False)}')
+    df_user_types = access_df_col(df, 'User Type')
+    if isinstance(df_user_types, pd.DataFrame):
+        print(f'\nCount of users per type: \n{df_user_types.value_counts(dropna= False).to_string(header= False)}')
 
 
     # Display counts of gender
-    user_per_gender = access_df_col(df, 'Gender')
-    if isinstance(user_per_gender, pd.DataFrame):
-        print(f'\nCount of users per gender: \n{user_per_gender.value_counts(dropna= False).to_string(header= False)}')
+    df_user_genders = access_df_col(df, 'Gender')
+    if isinstance(df_user_genders, pd.DataFrame):
+        print(f'\nCount of users per gender: \n{df_user_genders.value_counts(dropna= False).to_string(header= False)}')
 
 
     # Display earliest, most recent, and most common year of birth
-    user_birth_years = access_df_col(df, 'Birth Year')
-    if isinstance(user_birth_years, pd.DataFrame):
+    df_user_birth_years = access_df_col(df, 'Birth Year')
+    if isinstance(df_user_birth_years, pd.DataFrame):
         print('\nBirth year:')
-        print(f'Earliest: {user_birth_years.min().values[0].astype(int)}')
-        print(f'Latest: {user_birth_years.max().values[0].astype(int)}')
-        print(f'Most common: {user_birth_years.mode().values[0][0].astype(int)}')
+        print(f'Earliest: {df_user_birth_years.min().values[0].astype(int)}')
+        print(f'Latest: {df_user_birth_years.max().values[0].astype(int)}')
+        print(f'Most common: {df_user_birth_years.mode().values[0][0].astype(int)}')
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
@@ -202,13 +280,15 @@ def user_stats(df):
 
 
 def show_raw_data(df):
-    """ Asks if users wants to see a set amount of lines of raw data. And keeps repeating the question untill user does not say 'yes' or all the data has been shown. """
+    """ Asks if users wants to see a set amount of lines of raw data. And keeps repeating the question until user does not say 'yes' or all the data has been shown. """
     df_len = len(df.index)
     step = 5
-    start_line = 14334
+    start_line = 0
 
-    show = input(f'Would you like to see {step} lines of the raw data? Enter yes or no. : ')
-    while show.lower() == 'yes':
+    show = validatedInput(f'Would you like to see a set lines from the raw data? (yes/no, y/n) : ', ['yes', 'no', 'y', 'n', ''])
+
+    while show.lower() in ['yes', 'y']:
+        step = validatedIntInput('How many lines would you like to see (press Enter for default of 5): ', 5)
         end_line = start_line + step
 
         if end_line >= df_len:            
@@ -218,9 +298,10 @@ def show_raw_data(df):
         else:
             print(df.iloc[start_line : end_line].to_string())
             start_line = end_line
-            show = input(f'Would you like to see another set of {step} lines of data? Enter yes or no. : ')
+            show = validatedInput(f'Would you like to see another set of {step} lines of data? (yes/no, y/n) : ', ['yes', 'no', 'y', 'n', ''])
 
 
+    start_line = 0
     print('-'*40)
 
 
@@ -230,12 +311,14 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
-
-        show_raw_data(df)
+        if not df.empty:
+            time_stats(df)
+            station_stats(df)
+            trip_duration_stats(df)
+            user_stats(df)
+            show_raw_data(df)
+        else:
+            print('No data available!')
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-Python project and practice for the "Introduction to Python" course on udacity.
+Python project and practice for the "Introduction to Programming with Python I" course on udacity.
 
 Python version used: 3.14.2
 
@@ -6,4 +6,5 @@ Sources used outside of udacity:
     Pandas documentation - https://pandas.pydata.org/
         https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.style.html
     W3 school - Python - https://www.w3schools.com/python/
-    
+    Python documentation - time module - https://docs.python.org/3/library/time.html#module-time
+    Time conversion - https://sqlpey.com/python/top-4-ways-to-convert-seconds-to-hhmmss-in-python/
